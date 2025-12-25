eefe869ed2fc232a6ff61c439a4d6376b28d2510
Paul Sadowski <paul.j.sadowski@gmail.com>
2025-12-25 16:02:25 -0600

    Using the tess platform.

---
+++ src/Sadsol.POC.TesseractOCR/Sadsol.POC.TesseractOCR.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TesseractOCR;
+using TesseractOCR.Enums;
+
+Console.WriteLine("Hello, World!");
+using var engine = new Engine(@"./tessdata", Language.English, EngineMode.Default);
+using var img = TesseractOCR.Pix.Image.LoadFromFile(@"C:\Temp\Receipt_20241017_PGN.pdf");
+using var page = engine.Process(img);
+Console.WriteLine("Mean confidence: {0}", page.MeanConfidence);
+Console.WriteLine("Text: \r\n{0}", page.Text);
